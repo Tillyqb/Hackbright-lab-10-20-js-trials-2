c471bdcd59c83370e01c227996e5b24b1c596ad5
Matilda Q Basner <tillyqb@yahoo.com>
2020-10-20 23:24:35 +0000

    initial commit

---
+++ arrays.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+let i = 0
+for (item in items) {
+  console.log(item, i);
+  i++;
+}
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
