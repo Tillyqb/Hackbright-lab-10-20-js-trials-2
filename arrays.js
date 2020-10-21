1fd8894214179efedfa305473acfb4fb58d88f5e
Matilda Q Basner <tillyqb@yahoo.com>
2020-10-21 00:15:18 +0000

    arrays complete

--- arrays.js
+++ arrays.js
@@ -3,19 +3,29 @@
 
 // 1. printIndices
 function printIndices(items) {
-for (item in items) {
-  console.log(items[item], item);
-}
+  for (item in items) {
+    console.log(items[item], item);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = []
+  for (item in items){
+    if (item % 2 === 0){
+      result.push(items[item])
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  items.sort((a, b) => a - b);
+  sortedNItems = items.slice(0, n);
+  reverseSortedNItems = sortedNItems.sort((a, b) => b - a);
+
+  return reverseSortedNItems;
 }
