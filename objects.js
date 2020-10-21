ba9ada867a108a2062acad87bdbaba9dce5d511e
Matilda Q Basner <tillyqb@yahoo.com>
2020-10-21 01:03:22 +0000

    Lab complete

--- objects.js
+++ objects.js
@@ -3,11 +3,26 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+  words = phrase.split(' ');
+  for (word of words) {
+    if (!word.words) (
+      words[word] = 1;
+    ) else {
+      words[word] ++;
+    }
+  }
+  return wordCounts;
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    '2.50': ['Cantaloupe', 'Honeydew'],
+    '2.95': ['Watermelon'],
+    '3.25': ['Musk', 'Crenshaw'],
+    '14.25': ['Christmas']
+  }
+  return (melonPrices[price].sort());
 }
